6b118e1300e71f25f6638bc63cbd31ea3d5fe79c
codedefine <codedefine@gmail.com>
2012-01-27 17:38:23 +0600

    1. Updated schema of comment table to allow comment on event 2. Updated schema of talk table to support fulltext search 3. Updated event page to view and make comments 4. Added commentOnEvent function on commnetRepository

--- pages/comment.php
+++ pages/comment.php
@@ -1,8 +1,16 @@
 <?php
 
-if (!empty($_POST)) {
-    App::getRepository('Comment')->create($_POST);
-    header('Location: ' . ViewHelper::url('?page=talk&id=' . $_POST['talk_id'], true));
+if (!empty($_POST) && isset ($_SESSION['user'])) {
+	if(isset ($_POST['talk_id']))
+	{
+		App::getRepository('Comment')->create($_POST);
+		header('Location: ' . ViewHelper::url('?page=talk&id=' . $_POST['talk_id'], true));
+	}
+	elseif(isset($_POST['event_id']))
+	{
+		App::getRepository('Comment')->create($_POST);
+		header('Location: ' . ViewHelper::url('?page=event&id=' . $_POST['event_id'], true));
+	}
 } else {
     header('Location: ' . ViewHelper::url('', true));
 }
